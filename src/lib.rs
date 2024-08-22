a5eee21f012009bde8b28dacd1bc337f93234c1b
ddgos <ddgos@proton.me>
2024-08-22 12:55:00 +0100

    defined core types

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use vec2d::Vec2D;
+
+pub enum Player {
+    Black,
+    White,
+}
+
+pub type Intersection = Option<Player>;
+
+pub struct Edges {
+    pub north: bool,
+    pub east: bool,
+    pub south: bool,
+    pub west: bool,
+}
+
+pub struct Pattern {
+    pub edges: Edges,
+    pub pattern: Vec2D<Intersection>,
+}
+
+pub enum Rotation {
+    None,
+    Quarter,
+    Half,
+    ThreeQuarters,
+}
+
+pub struct PatternVariator {
+    pub swap_colours: bool,
+    pub reflect: bool,
+    pub rotation: Rotation,
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
